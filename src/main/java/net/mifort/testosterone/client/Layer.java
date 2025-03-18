da0d84e0e43a8ed4da9cc93406c8f6faeeabb8dc
mifort <mifort.github@gmail.com>
2025-03-18 21:53:29 +0100

    beard done

--- src/main/java/net/mifort/testosterone/client/Layer.java
+++ src/main/java/net/mifort/testosterone/client/Layer.java
@@ -2,7 +2,6 @@ package net.mifort.testosterone.client;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
-import net.mifort.testosterone.effects.testosteroneModEffects;
 import net.mifort.testosterone.network.packet.effectCheckerC2SPacket;
 import net.mifort.testosterone.network.testosteroneModMessages;
 import net.mifort.testosterone.testosterone;
@@ -38,7 +37,7 @@ public class Layer extends RenderLayer<AbstractClientPlayer, PlayerModel<Abstrac
             VertexConsumer vBuff = pBuffer.getBuffer(RenderType.entityTranslucent(beardTexture));
             pPoseStack.pushPose();
             pPoseStack.translate(0, 0, 0); // move the model
-            pPoseStack.scale(1.1f, 1.1f, 1.1f);
+            pPoseStack.scale(1f, 1f, 1f);
             getParentModel().head.translateAndRotate(pPoseStack);
             BeardModel.renderToBuffer(pPoseStack, vBuff, pPackedLight, OverlayTexture.NO_OVERLAY, 1f, 1f, 1f, 1f);
             pPoseStack.popPose();
--- src/main/java/net/mifort/testosterone/client/beardModel.java
+++ src/main/java/net/mifort/testosterone/client/beardModel.java
@@ -15,7 +15,7 @@ import net.minecraft.resources.ResourceLocation;
 
 public class beardModel extends Model {
 	// This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
-	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("modid", "beardmodel"), "main");
+	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("modid", "beardmodel2"), "main");
 	private final ModelPart bb_main;
 
 	public beardModel(ModelPart root) {
@@ -27,11 +27,23 @@ public class beardModel extends Model {
 		MeshDefinition meshdefinition = new MeshDefinition();
 		PartDefinition partdefinition = meshdefinition.getRoot();
 
-		PartDefinition bb_main = partdefinition.addOrReplaceChild("bb_main", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -32.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
+		PartDefinition bb_main = partdefinition.addOrReplaceChild("bb_main", CubeListBuilder.create().texOffs(4, 17).addBox(-2.0F, -27.0F, -5.0F, 4.0F, 1.0F, 1.0F, new CubeDeformation(0.0F))
+				.texOffs(19, 18).addBox(-4.0F, -26.0F, -5.0F, 2.0F, 3.0F, 1.0F, new CubeDeformation(0.0F))
+				.texOffs(19, 18).mirror().addBox(2.0F, -26.0F, -5.0F, 2.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)).mirror(false)
+				.texOffs(5, 3).addBox(-3.0F, -23.0F, -5.0F, 6.0F, 1.0F, 1.0F, new CubeDeformation(0.0F))
+				.texOffs(27, 16).addBox(3.25F, -28.75F, -3.25F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F))
+				.texOffs(26, 18).addBox(3.25F, -27.75F, -4.25F, 1.0F, 2.0F, 2.0F, new CubeDeformation(0.0F))
+				.texOffs(27, 22).addBox(3.25F, -25.75F, -4.25F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F))
+				.texOffs(26, 18).addBox(-4.25F, -27.75F, -4.25F, 1.0F, 2.0F, 2.0F, new CubeDeformation(0.0F))
+				.texOffs(27, 16).addBox(-4.25F, -28.75F, -3.25F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F))
+				.texOffs(27, 22).addBox(-4.25F, -25.75F, -4.25F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F))
+				.texOffs(20, 28).addBox(-2.0F, -22.0F, -5.0F, 4.0F, 1.0F, 1.0F, new CubeDeformation(0.0F))
+				.texOffs(4, 22).addBox(-2.0F, -25.0F, -5.0F, 4.0F, 2.0F, 1.0F, new CubeDeformation(0.0F))
+				.texOffs(0, 23).addBox(1.0F, -26.0F, -5.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F))
+				.texOffs(0, 23).addBox(-2.0F, -26.0F, -5.0F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
 
 		return LayerDefinition.create(meshdefinition, 32, 32);
 	}
-
 
 	@Override
 	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
