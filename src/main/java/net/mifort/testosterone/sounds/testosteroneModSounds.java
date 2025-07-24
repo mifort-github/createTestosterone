028adb64162056d303db5fca83cdc74ba79de7b7
Jo0oN <68225410+JonMyDude@users.noreply.github.com>
2025-07-24 17:21:30 +0200

    sound prep for mifort
    hue shift pzza man
    sfx prep

--- src/main/java/net/mifort/testosterone/sounds/testosteroneModSounds.java
+++ src/main/java/net/mifort/testosterone/sounds/testosteroneModSounds.java
@@ -13,6 +13,11 @@ public class testosteroneModSounds {
 
     public static final RegistryObject<SoundEvent> JOHN_ROCK_DEACTIVATION = registerSoundEvents("john_rock_deactivation");
     public static final RegistryObject<SoundEvent> JOHN_ROCK_ACTIVATION = registerSoundEvents("john_rock_activation");
+    public static final RegistryObject<SoundEvent> MACH_1_SFX = registerSoundEvents("mach_1_sfx");
+    public static final RegistryObject<SoundEvent> MACH_2_SFX = registerSoundEvents("mach_2_sfx");
+    public static final RegistryObject<SoundEvent> GROUND_SLAM_SFX = registerSoundEvents("ground_slam_sfx");
+    public static final RegistryObject<SoundEvent> ENEMY_HIT_SFX = registerSoundEvents("enemy_hit_sfx");
+
 
     private static RegistryObject<SoundEvent> registerSoundEvents(String name) {
         return SOUND_EVENTS.register(name, () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(testosterone.MOD_ID, name)));
