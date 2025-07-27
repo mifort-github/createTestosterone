41091117d887e4b795916afc4b471f88d7db4ba8
Jo0oN <68225410+JonMyDude@users.noreply.github.com>
2025-07-27 15:33:00 +0200

    sound prep 2
    rat sounds

--- src/main/java/net/mifort/testosterone/sounds/testosteroneModSounds.java
+++ src/main/java/net/mifort/testosterone/sounds/testosteroneModSounds.java
@@ -18,6 +18,13 @@ public class testosteroneModSounds {
     public static final RegistryObject<SoundEvent> GROUND_SLAM_SFX = registerSoundEvents("ground_slam_sfx");
     public static final RegistryObject<SoundEvent> ENEMY_HIT_SFX = registerSoundEvents("enemy_hit_sfx");
 
+    public static final RegistryObject<SoundEvent> RAT_SNIFF1 = registerSoundEvents("rat_sniff1");
+    public static final RegistryObject<SoundEvent> RAT_SNIFF2 = registerSoundEvents("rat_sniff2");
+    public static final RegistryObject<SoundEvent> RAT_SQUEAK1 = registerSoundEvents("rat_squeak1");
+    public static final RegistryObject<SoundEvent> RAT_SQUEAK2 = registerSoundEvents("rat_squeak2");
+    public static final RegistryObject<SoundEvent> RAT_RUN = registerSoundEvents("rat_run");
+    public static final RegistryObject<SoundEvent> RAT_HURT1 = registerSoundEvents("rat_hurt1");
+    public static final RegistryObject<SoundEvent> RAT_HURT2 = registerSoundEvents("rat_hurt2");
 
     private static RegistryObject<SoundEvent> registerSoundEvents(String name) {
         return SOUND_EVENTS.register(name, () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(testosterone.MOD_ID, name)));
